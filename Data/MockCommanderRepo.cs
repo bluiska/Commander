bc87bf92b57f55fb6d7b761efe20f44699458ca2
bluiska <bluiska@gmail.com>
2020-06-12 17:37:58 +0100

    Basic API
    Basic GET call for all commands and commands by ID implemented. Work will continue later when I get time...

--- Data/MockCommanderRepo.cs
+++ Data/MockCommanderRepo.cs
@@ -10,13 +10,14 @@ namespace Commander.Data
             return new List<Command>
             {
                 new Command { Id = 0, HowTo = "Boil and egg", Line = "Boil water", Platform = "Kettle & Pan" },
-
+                new Command { Id = 0, HowTo = "Cut bread", Line = "Get a knife", Platform = "Knife & Chopping board" },
+                new Command { Id = 0, HowTo = "Make a cuppa", Line = "Place teabag in cup", Platform = "Kettle & Cup" },
             };
         }
 
         public Command GetCommandById(int id)
         {
-            throw new System.NotImplementedException();
+            return new Command { Id = 0, HowTo = "Boil and egg", Line = "Boil water", Platform = "Kettle & Pan" };
         }
     }
 }
